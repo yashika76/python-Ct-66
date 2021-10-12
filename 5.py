c45e8eedf0e80bbc9c2468dfec7de68b6d3f5523
yashika76 <81283115+yashika76@users.noreply.github.com>
2021-10-13 00:09:47 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,9 @@
+fruit1 = input("Enter 1st fruit name : ")
+fruit2 = input("Enter 2nd fruit name : ")
+fruit3 = input("Enter 3rd fruit name : ")
+fruit4 = input("Enter 4th fruit name : ")
+fruit5 = input("Enter 5th fruit name : ")
+
+fruitList = [fruit1, fruit2, fruit3, fruit4, fruit5]
+
+print(fruitList)
