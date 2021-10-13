07e5028a9c57e7d7e6cd76f45bc1d22b8e003cf9
yashika76 <81283115+yashika76@users.noreply.github.com>
2021-10-13 12:19:33 +0530

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,10 @@
+first = int(input("enter first number : "))
+second = int(input("enter first number : "))
+third = int(input("enter first number : "))
+fourth = int(input("enter first number : "))
+
+numberList = [first, second, third, fourth]
+
+sum = numberList[0] + numberList[1] + numberList[2] + numberList[3]
+
+print(sum)
