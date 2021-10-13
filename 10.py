bb857ed40f37249f3287acd28620c52a3408fc20
yashika76 <81283115+yashika76@users.noreply.github.com>
2021-10-13 12:23:06 +0530

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,14 @@
+myDict = {
+
+    "Pankha" : "Fan", 
+    "Vastu" : "Item",
+    "Aacha" : "Good",
+    "Adbhut" : "Tremendous",
+    "Vishaal" : "Huge",
+}
+
+print("Options are : ", myDict.keys())
+
+a = input("Enter Your Word : ")
+
+print("The meaning of your word is : ", myDict[a])
